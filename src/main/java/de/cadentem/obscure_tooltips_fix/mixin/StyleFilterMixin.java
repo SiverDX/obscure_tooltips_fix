a886d57e9913c8a22424117592af0b8b4a89fb40
Kai <kai.zahn@yahoo.de>
2025-07-31 23:54:04 +0200

    add support for item tags

--- src/main/java/de/cadentem/obscure_tooltips_fix/mixin/StyleFilterMixin.java
+++ src/main/java/de/cadentem/obscure_tooltips_fix/mixin/StyleFilterMixin.java
@@ -3,27 +3,41 @@ package de.cadentem.obscure_tooltips_fix.mixin;
 import com.google.gson.JsonObject;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.mojang.serialization.JsonOps;
 import com.obscuria.tooltips.client.style.StyleFilter;
+import de.cadentem.obscure_tooltips_fix.OTF;
 import de.cadentem.obscure_tooltips_fix.utils.NBTFilter;
 import de.cadentem.obscure_tooltips_fix.utils.NBTUtils;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-import javax.annotation.Nullable;
+import java.util.List;
 
 @Mixin(value = StyleFilter.class, remap = false)
 public abstract class StyleFilterMixin implements NBTFilter {
     @Unique @Nullable public CompoundTag obscure_tooltips_fix$NBT;
+
+    @Unique @Nullable public List<TagKey<Item>> obscure_tooltips_fix$tags;
 
     @ModifyReturnValue(method = "fromJson", at = @At("RETURN"))
     private static StyleFilter setObscure_tooltips_fix$parseTag(final StyleFilter filter, @Local(argsOnly = true) final JsonObject root) {
         if (root.has("tag")) {
             CompoundTag tag = NBTUtils.toCompound(root.getAsJsonObject("tag"));
             ((NBTFilter) filter).obscure_tooltips_fix$setTag(tag);
+        }
+
+        if (root.has("item_tags")) {
+            TagKey.codec(Registries.ITEM).listOf().decode(JsonOps.INSTANCE, root.getAsJsonArray("item_tags"))
+                    .resultOrPartial(OTF.LOG::error)
+                    .ifPresent(result -> ((NBTFilter) filter).obscure_tooltips_fix$setTags(result.getFirst()));
         }
 
         return filter;
@@ -43,6 +57,19 @@ public abstract class StyleFilterMixin implements NBTFilter {
         }
     }
 
+    @ModifyReturnValue(method = "test", at = @At("RETURN"))
+    private boolean obscure_tooltips_fix$testItems(final boolean isValid, @Local(argsOnly = true) final ItemStack stack) {
+        if (obscure_tooltips_fix$tags != null && !obscure_tooltips_fix$tags.isEmpty()) {
+            for (TagKey<Item> tag : obscure_tooltips_fix$tags) {
+                if (!stack.getItemHolder().is(tag)) {
+                    return false;
+                }
+            }
+        }
+
+        return isValid;
+    }
+
     @Override
     public void obscure_tooltips_fix$setTag(final CompoundTag tag) {
         obscure_tooltips_fix$NBT = tag;
@@ -51,5 +78,10 @@ public abstract class StyleFilterMixin implements NBTFilter {
     @Override
     public @Nullable CompoundTag obscure_tooltips_fix$getTag() {
         return obscure_tooltips_fix$NBT;
+    }
+
+    @Override
+    public void obscure_tooltips_fix$setTags(final List<TagKey<Item>> tags) {
+        this.obscure_tooltips_fix$tags = tags;
     }
 }
--- src/main/java/de/cadentem/obscure_tooltips_fix/utils/NBTFilter.java
+++ src/main/java/de/cadentem/obscure_tooltips_fix/utils/NBTFilter.java
@@ -1,10 +1,15 @@
 package de.cadentem.obscure_tooltips_fix.utils;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public interface NBTFilter {
     @Nullable CompoundTag obscure_tooltips_fix$getTag();
     void obscure_tooltips_fix$setTag(final CompoundTag tag);
+
+    void obscure_tooltips_fix$setTags(final List<TagKey<Item>> tags);
 }
